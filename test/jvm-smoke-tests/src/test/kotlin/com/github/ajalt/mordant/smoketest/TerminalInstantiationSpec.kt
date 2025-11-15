d8c6478c8f248f1623a095f7b98ae32685b5bb64
Robert Elliot <rob@lidalia.org.uk>
2025-11-15 12:19:50 +0000

    chore: Run the built jar against Java LTS versions

--- test/jvm-smoke-tests/src/test/kotlin/com/github/ajalt/mordant/smoketest/TerminalInstantiationSpec.kt
+++ test/jvm-smoke-tests/src/test/kotlin/com/github/ajalt/mordant/smoketest/TerminalInstantiationSpec.kt
@@ -1,14 +1,13 @@
 package com.github.ajalt.mordant.smoketest
 
 import com.github.ajalt.mordant.terminal.Terminal
-import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
 class TerminalInstantiationSpec : StringSpec({
 
     "can instantiate a Terminal" {
-        shouldNotThrowAny {
-            Terminal()
-        }
+        val terminal = Terminal()
+        terminal.terminalInfo.interactive shouldBe true
     }
 })
